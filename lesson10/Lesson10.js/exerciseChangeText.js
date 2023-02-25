e12b9885532e8aee91e6bf3c724eaf0ffc61f4eb
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-02-25 19:26:09 +0200

    add lesson 10

---
+++ lesson10/Lesson10.js/exerciseChangeText.js
@@ -0,0 +1,7 @@
+document.getElementById("p2-name").textContent("MITE4ETO")
+document.getElementById("p1-name").textContent("Kolio")
+	
+document.getElementById("p1-symbol").textContent = "O"
+document.getElementById("p2-symbol").textContent = "X"
+      
+document.title = "A game you know and love"
