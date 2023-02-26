19d808d8380c02becce9125296ef2b3363acda67
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-02-26 11:05:45 +0200

    add operators.js

---
+++ lesson15.js/capitalize.js
@@ -0,0 +1,6 @@
+document.querySelector("header h1").style.textTransform = "uppercase"
+// promenq na glavni bukvi
+
+document.querySelector("header h1").textContent = 
+document.querySelector("header h1").textContent.toUpperCase()
+// promenq toUpperCase
