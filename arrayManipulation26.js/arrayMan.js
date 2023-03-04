65077c0794b19eded01aedd074a8edbd1cbfed9f
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-03-04 18:08:28 +0200

    added array manipulation

---
+++ arrayManipulation26.js/arrayMan.js
@@ -0,0 +1,18 @@
+// we do understand how to manipulate an array, we can replace an element by using synonyms[1] = "variety"
+
+// we can use let lastItem = synonyms.pop()   - in this way we do remove the last element from the array table in this case is cornucopia
+// we can use synonyms.push("multitude")     - in this way we do add one more element into the array table located in the last position
+
+let synonyms = ["plethora","array","cornucopia"];
+
+synonyms[1] = "variety";
+
+let lastItem = synonyms.pop();
+
+synonyms.push("multitude");
+
+console.log(synonyms)
+
+
+
+
