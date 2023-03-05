4440d708f036fbc1b98724dd00ee0710932c040a
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-03-05 12:32:16 +0200

    add mutable/immutable
    learned about mutable and unmutable values
    we can mutate the data in the arrays but we cannot in the string

---
+++ arrayManipulation26.js/IMMUTABLE.js
@@ -0,0 +1,7 @@
+// immutable value - we cannot cange the data in the string 
+
+let abcString = "abc"
+
+abcString[1] = "O"
+
+console.log(abcString)
---
+++ arrayManipulation26.js/MUTABLE.js
@@ -0,0 +1,9 @@
+ // mutable value - we can change the data in the array and the outcome will be new, changed one
+ // immutable value - we cannot cange the data in the string 
+
+
+let abcArray = ["a","b","c"]
+
+abcArray[1] = "O"
+
+console.log(abcArray)
