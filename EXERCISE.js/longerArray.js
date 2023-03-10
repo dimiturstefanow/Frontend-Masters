66aab72ea3c38df86584905c5283f3664dfe229c
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-03-10 21:59:03 +0200

    add multiply, add longer longerArray

---
+++ EXERCISE.js/longerArray.js
@@ -0,0 +1,11 @@
+function longerThan(a1,a2){
+    return a1.length, a2.length;
+
+    if (a1.length > a2.length){
+        return true;
+    } else {
+        return false
+    }
+} 
+
+console.log(longerThan([1,2,3,4,5,6],[1,2,3,4,5]))
