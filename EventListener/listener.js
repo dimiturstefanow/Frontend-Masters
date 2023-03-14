7f64ba23c825bc7c6369ab93190374c36f3bca94
MITE4ETO <dimitrstefanov7v2@gmail.com>
2023-03-14 08:58:10 +0200

    add event listener

---
+++ EventListener/listener.js
@@ -0,0 +1,5 @@
+document.addEventListener("click", (event) => {
+    console.log(event.target);
+});
+
+// we can use this thing and the structure for https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events
